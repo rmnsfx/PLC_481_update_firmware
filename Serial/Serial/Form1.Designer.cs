9c5bf5d7d3dcd21bf411a8e83d0a3ae41fa8e645
rms <rooqmeansquare@yandex.ru>
2017-10-26 14:03:26 +0500

    configured UI, combobox for com settings, progress bar

--- Serial/Serial/Form1.Designer.cs
+++ Serial/Serial/Form1.Designer.cs
@@ -31,49 +31,157 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 210);
+            this.button1.Location = new System.Drawing.Point(379, 225);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Загрузка";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(115, 210);
+            this.button2.Location = new System.Drawing.Point(203, 225);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(118, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Стереть FLASH";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(220, 210);
+            this.button3.Location = new System.Drawing.Point(22, 225);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(175, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Перейти в режим загрузчика";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 175);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(432, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(379, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Открыть";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(149, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Настройки COM порта";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Файл прошивки";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Адрес ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 262);
+            this.ClientSize = new System.Drawing.Size(476, 280);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +190,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Serial/Serial/Form1.cs
+++ Serial/Serial/Form1.cs
@@ -17,27 +17,79 @@ using System.Text;
 using System.Windows;
 using System.IO.Ports;
 using System.Threading;
-using System.ComponentModel;
+
 
 namespace Serial
 {
     public partial class Form1 : Form
     {
+        private delegate void SetProgressBarValue(int value);
+
+
+        int i = 0;
+        SerialPort serial = new SerialPort();
+        
+        Thread thread = null;
+        Thread thread_2 =  null;
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        string file_path = null;
+        int file_lenght = 0;
+        FileStream fs;
+
+        string selectedValue_comPort = "COM5";
+        int selectedValue_baudRate = 115200;
+
+        int modbus_adr = 10; 
+
         public Form1()
         {
             InitializeComponent();
+
+            var ports = SerialPort.GetPortNames();
+            comboBox1.DataSource = ports;
+            comboBox1.SelectedIndex = 1;
+            comboBox2.Items.Add("9600"); 
+            comboBox2.Items.Add("115200");
+            comboBox2.SelectedIndex = 1;
+
+            //serial.PortName = "COM5";
+            //serial.BaudRate = 115200;
+            //serial.Handshake = System.IO.Ports.Handshake.None;
+            //serial.Parity = Parity.None;
+            //serial.DataBits = 8;
+            //serial.StopBits = StopBits.One;
+            //serial.ReadTimeout = 1000;
+            //serial.WriteTimeout = 5000;
+
+            textBox2.Text += Convert.ToString(modbus_adr);
         }
 
-        SerialPort serial = new SerialPort();
-        FileStream fs = new FileStream(@"D:\Repos\PLC_1xx\plc_1xx_L433\MDK-ARM\PLC_1xx.bin", FileMode.Open);
-        
-
-        bool SerialOpen()
+        private void prog_bar()
         {
-            try
+            while (progressBar1.Value < progressBar1.Maximum)
             {
-                serial.PortName = "COM5";
-                serial.BaudRate = 115200;
+                if (InvokeRequired)
+                {
+                    this.Invoke(new SetProgressBarValue(SetProgressValue), i);
+                }            
+            }
+
+            System.Threading.Thread.Sleep(800);
+            MessageBox.Show("Прошивка завершена");
+
+        }
+
+        public void SerialOpen()
+        {
+            i = 0;
+            int status = 0;
+
+            try
+            {                
+                
+                serial.PortName = selectedValue_comPort;
+                serial.BaudRate = selectedValue_baudRate;
                 serial.Handshake = System.IO.Ports.Handshake.None;
                 serial.Parity = Parity.None;
                 serial.DataBits = 8;
@@ -45,52 +97,66 @@ namespace Serial
                 serial.ReadTimeout = 1000;
                 serial.WriteTimeout = 5000;
 
+
                 serial.Open();
 
-                //int len = (int)fs.Length;
-                
-                byte[] bytestosend = new byte[45808];
+                byte[] bytestosend = new byte[file_lenght];
+ 
+                serial.DiscardOutBuffer();
+                serial.DiscardInBuffer();
 
-                //serial.Write(erase_command, 0, 2);
-                //serial.DiscardOutBuffer();
-                //serial.DiscardInBuffer();
-                //System.Threading.Thread.Sleep(5);
-                
-                fs.Read(bytestosend, 0, 45808);
-                                
-                for (int i = 0; i < bytestosend.Length; i += 8)
+                status = fs.Read(bytestosend, 0, file_lenght);
+                if (status == 0)
                 {
-                    serial.DiscardOutBuffer();
-                    serial.DiscardInBuffer();
-                    serial.Write(bytestosend, i, 8);
-
-                    System.Threading.Thread.Sleep(3); 
+                    MessageBox.Show("Ошибка чтения файла прошивки");
                 }
+                else 
+                { 
+                    for (i = 0; i < bytestosend.Length; i += 8)
+                    {
+                        serial.DiscardOutBuffer();
+                        serial.DiscardInBuffer();
+                        serial.Write(bytestosend, i, 8);
+
+                        System.Threading.Thread.Sleep(3);                    
+                    }                    
+                }
+
+                fs.Position = 0;
 
                 serial.Close();
                 //serial.Dispose();
                 //serial = null;
-
-                return true;
+                
             }
             catch
             {
-                return false;
+                MessageBox.Show("Ошибка открытия COM порта");
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+            thread = new Thread(new ThreadStart(SerialOpen));
+            thread.IsBackground = true;
+            thread.Start();
 
-            SerialOpen();
+            progressBar1.Value = 0;
+            progressBar1.Maximum = file_lenght;
 
+            
+            thread_2 = new Thread(new ThreadStart(prog_bar));
+            thread_2.IsBackground = true;
+            thread_2.Start();
+            
         }
 
         private void button2_Click(object sender, System.EventArgs e)
         {
-            serial.PortName = "COM5";
-            serial.BaudRate = 115200;
+
+            serial.PortName = selectedValue_comPort;
+            serial.BaudRate = selectedValue_baudRate;
             serial.Handshake = System.IO.Ports.Handshake.None;
             serial.Parity = Parity.None;
             serial.DataBits = 8;
@@ -108,8 +174,9 @@ namespace Serial
 
         private void button3_Click(object sender, System.EventArgs e)
         {
-            serial.PortName = "COM5";
-            serial.BaudRate = 115200;
+
+            serial.PortName = selectedValue_comPort;
+            serial.BaudRate = selectedValue_baudRate;
             serial.Handshake = System.IO.Ports.Handshake.None;
             serial.Parity = Parity.None;
             serial.DataBits = 8;
@@ -117,12 +184,68 @@ namespace Serial
             serial.ReadTimeout = 1000;
             serial.WriteTimeout = 5000;
 
-            byte[] erase_command = new byte[] {10, 98, 111, 111,  116};
+            byte[] erase_command = new byte[] {Convert.ToByte(modbus_adr), 98, 111, 111,  116};
             serial.Open();
             serial.DiscardOutBuffer();
             serial.DiscardInBuffer();
             serial.Write(erase_command, 0, erase_command.Length);
             serial.Close();
         }
+
+        private void progressBar1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void SetProgressValue(int value)
+        {            
+            progressBar1.Value = value;
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+
+
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text += openFileDialog1.FileName;
+                file_path = openFileDialog1.FileName;
+                fs = new FileStream(@file_path, FileMode.Open);
+                file_lenght = (int)fs.Length;
+                label1.Text = file_lenght.ToString();
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                selectedValue_comPort = (string)comboBox1.SelectedValue;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+            if (comboBox2.SelectedItem != null)
+            {
+                Int32.TryParse(comboBox2.SelectedItem.ToString(), out selectedValue_baudRate);
+            }            
+                       
+        }
+
+        private void textBox2_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+
     }
 }
